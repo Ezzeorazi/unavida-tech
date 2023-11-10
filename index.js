9421ac338e7d6055c232f9cbfa9b8dd228e3cb6f
Ezzeorazi <ezequiel.orazi90@gmail.com>
2023-11-10 20:20:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    var menu = document.getElementById("sideMenu");
+    if (menu.style.width === '0px' || menu.style.width === '') {
+        menu.style.width = '250px'; // Ajusta este valor según el ancho deseado para tu menú
+    } else {
+        menu.style.width = '0px';
+    }
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 3000); // Cambia cada 3 segundos
+}
